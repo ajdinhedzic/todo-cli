bef94423a236802ea758ef298a060ab5ae221d75
Ajdin <ahedzic@gmail.com>
2016-03-11 21:47:59 -0600

    Adding ability to add items to list

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
+var program = require ('commander');
+var fs = require('fs');
 
+var file = 'todo.json';
+var list = JSON.parse(fs.readFileSync(file, 'utf8'));
 
-console.log("hello");
+program
+	.version('0.0.1')
+	.option('-a, --add <item>', 'add item to list', function(item){
+		list.push(item);
+	})
+	.parse(process.argv);
+
+fs.writeFile(file, JSON.stringify(list), 'utf8');
