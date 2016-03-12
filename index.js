893c61d43467caa23d82ec6529a9c0e3e00ebfad
Ajdin <ahedzic@gmail.com>
2016-03-12 10:59:40 -0600

    Refactoring out options into separate programs

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ var file = 'todo.json';
 var list = JSON.parse(fs.readFileSync(file, 'utf8'));
 
 program
-	.version('0.0.1')
-	.option('-a, --add <item>', 'add item to list', function(item){
+	.command('add <item>')
+	.description('Add a todo item to the existing list')
+	.action(function(item){
+		console.log("adding item: %s", item);
 		list.push(item);
 	})
-	.option('-l, --list', 'shows all items in the list', function(){
-		console.log(list);
-	})
-	.option('-r, --remove <index>', 'remove item at index, 0 based', function(index){
+
+program
+	.command('remove <index>')
+	.description('remove item at index, 0 based')
+	.action(function(index){
 		list.splice(index, 1);
 	})
-	.parse(process.argv);
+
+program.parse(process.argv);
 
 fs.writeFile(file, JSON.stringify(list), 'utf8');
 
