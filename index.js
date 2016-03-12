d12ea4d4e40b856a6e5f121613790f4cc771bf26
Ajdin <ahedzic@gmail.com>
2016-03-11 21:56:14 -0600

    ability to remove elements

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ program
 	.option('-a, --add <item>', 'add item to list', function(item){
 		list.push(item);
 	})
+	.option('-l, --list', 'shows all items in the list', function(){
+		console.log(list);
+	})
+	.option('-r, --remove <index>', 'remove item at index, 0 based', function(index){
+		list.splice(index, 1);
+	})
 	.parse(process.argv);
 
 fs.writeFile(file, JSON.stringify(list), 'utf8');
+
+console.log(list);
